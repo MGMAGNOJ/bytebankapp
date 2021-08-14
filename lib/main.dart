f31ac20efd374706d5e31d7c1558461ca6224d13
Mario G M Magno Jr <mario.magno@acanto.ca>
2021-08-13 22:09:28 -0300

    Curso 2 Aula 8

--- lib/main.dart
+++ lib/main.dart
@@ -12,27 +12,59 @@ class MyApp extends StatelessWidget {
       title: 'ByteBankApp',
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        accentColor: Colors.blueAccent.shade700,
       ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Dashboard ByteBankApp"),
-        ),
-        body: Column(
-          children: <Widget>[
-            Image.asset("images/bytebank_logo.png"),
-            Container(
-              height: 200,
-              width: 200,
+      home: Dashboard(),
+    );
+  }
+}
+
+class Dashboard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Dashboard ByteBankApp"),
+      ),
+      body: Column(
+        // Alinhamento entre os componentes
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        // Alinhamento Horizontal
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Image.asset("images/bytebank_logo.png"),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              // Padding interno do container
+              padding: EdgeInsets.all(8.0),
+              height: 120,
+              width: 120,
               color: Colors.green,
               child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Icon(Icons.people),
-                  Text("Texto do Copntainer"),
+                  Icon(
+                    Icons.people,
+                    color: Colors.white,
+                    size: 24.0,
+                  ),
+                  Text(
+                    'Contatos',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16.0,
+                    ),
+                  ),
                 ],
               ),
-            )
-          ],
-        ),
+            ),
+          )
+        ],
       ),
     );
   }
